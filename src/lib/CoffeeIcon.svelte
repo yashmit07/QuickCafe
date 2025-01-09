2923234ee1088a317f7f635a33ef9ddebd127314
Yashmit Singh <yashmit.singh.02@gmail.com>
2025-01-09 15:52:45 -0800

    Created coffee icon for café theme

---
+++ src/lib/CoffeeIcon.svelte
@@ -0,0 +1,31 @@
+<div>
+    <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M18 8h1a4 4 0 1 1 0 8h-1"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M6 1v3M10 1v3M14 1v3"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+    </svg>
+</div>
