31e5c9d179bab66ec9e29db6db3fcd04e3ebe157
Yashmit Singh <yashmit.singh.02@gmail.com>
2025-01-22 21:50:15 -0800

    updated variables in supabase.ts

--- src/lib/db/supabase.ts
+++ src/lib/db/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } from '$env/static/private'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
-export const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY)
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
 
 export async function verifyDatabaseSetup() {
     try {
